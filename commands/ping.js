047a9ccd20d1921b4c32123e1703400dc2f68c8e
GreDDySS <misha.sudos@yandex.ru>
2023-04-25 01:17:20 +0800

    Set command ping

---
+++ commands/ping.js
@@ -0,0 +1,15 @@
+exports.run = async (client, chat, channel, commandData) => {
+    const used = process.memoryUsage().heapUsed / 1024 / 1024
+    const memory = Math.round(used * 100) / 100
+    const channelCount = await greddBot.Channel.getJoinable()
+    
+    chat.send(channel, `@${commandData.user.name} PONG! 🟢  Channels: ${channelCount.length} | Uptime: ${greddBot.Utils.misc.uptime()} | Memory: ${memory}MB/512MBs | Cmd executed: ${greddBot.Utils.temp.cmdCount}`)
+}
+module.exports.config = {
+    name: "ping",
+    description: "Check status bot",
+    cooldown: 5000,
+    aliases: ["ping"],
+    active: true,
+    adminOmnly: false
+}
