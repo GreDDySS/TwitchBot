a9b841e8dc28fedfd82fc6c8fdf16978870c630f
GreDDySS <misha.sudos@yandex.ru>
2023-04-26 18:17:37 +0800

    add check ping twitch

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,12 @@
 exports.run = async (client, chat, channel, commandData) => {
+    let dateNow = Date.now()
+    await client.ping();
+    let dateAfterPing = Date.now()
     const used = process.memoryUsage().heapUsed / 1024 / 1024
     const memory = Math.round(used * 100) / 100
     const channelCount = await greddBot.Channel.getJoinable()
     
-    chat.send(channel, `@${commandData.user.name} PONG! 🟢  Channels: ${channelCount.length} | Uptime: ${greddBot.Utils.misc.uptime()} | Memory: ${memory}MB/512MBs | Cmd executed: ${greddBot.Utils.temp.cmdCount}`)
+    chat.send(channel, `@${commandData.user.name} PONG! 🟢 Ping: ${dateAfterPing - dateNow}ms | Channels: ${channelCount.length} | Uptime: ${greddBot.Utils.misc.uptime()} | Memory: ${memory}MB/512MBs | Cmd executed: ${greddBot.Utils.temp.cmdCount}`)
 }
 module.exports.config = {
     name: "ping",
