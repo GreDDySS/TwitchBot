567d77f50c6e17b5969e4e2b8d14130ed5105317
GreDDySS <misha.sudos@yandex.ru>
2023-04-30 21:22:05 +0800

    Leave channel

---
+++ commands/leave.js
@@ -0,0 +1,24 @@
+exports.run = async (client, chat, channel, commandData) => {
+    const arg = commandData.message.args
+    if (commandData.user.badges.hasBroadcaster) {
+        if (arg.length > 0) return
+        await greddBot.DB.db.query(`Delete from channel where "userId" = '${commandData.user.id}'`)
+        client.part(commandData.user.name)
+    }
+
+    if (commandData.user.login == greddBot.Config.owner) {
+        const uid = await greddBot.Utils.APITwitch.getLoginInfo(arg[0])
+        const result = uid.map((item) => {return item.id})
+        await greddBot.DB.db.query(`Delete from channel where "userId" = '${result}'`)
+    } else {
+        chat.send(channel, `@${commandData.user.name}, У вас нет прав.`)
+    }
+}
+module.exports.config = {
+    name: "test",
+    description: "Leave channel",
+    cooldown: 666,
+    aliases: [],
+    active: true,
+    adminOmnly: false
+}
