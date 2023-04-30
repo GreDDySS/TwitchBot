ee7c4bfe2d73e451997cbed655ba71f117ad1b07
GreDDySS <misha.sudos@yandex.ru>
2023-04-30 22:59:13 +0800

    Change stream title

---
+++ commands/title.js
@@ -0,0 +1,40 @@
+const pc = require("picocolors")
+exports.run = async (client, chat, channel, commandData) => {
+    const msgArgs = commandData.message.args
+    const isMod = commandData.user.badges.hasModerator
+    const isBroad = commandData.user.badges.hasBroadcaster
+    try {
+        if (msgArgs && (isMod || isBroad) && greddBot.Config.owner ) {
+
+            await greddBot.Utils.ApiClient.helix(`channels?broadcaster_id=176257472`, {
+                method: "PATCH",
+                data: {
+                    title: msgArgs
+                },
+            }).then(() => {
+                chat.send(channel, `@${commandData.user.name}, Название успешно обновлено!`)
+            }).catch((error) => {
+                console.log(error)
+            })
+            
+        } else {
+            await greddBot.Utils.ApiClient.helix(`channels?broadcaster_id=176257472`)
+            .then(async (response) => {
+                const title = response.data[0].title
+                chat.send(channel, `Название стрима: "${title}"`)
+            })
+        }
+
+    } catch (error) {
+        console.log(error)
+        greddBot.Logger.error(`${pc.red(`[ERROR]`)} || Error: ${error}`)
+    }
+}
+module.exports.config = {
+    name: "title",
+    description: "set tittle stream",
+    cooldown: 5000,
+    aliases: [],
+    active: true,
+    adminOmnly: false
+}
