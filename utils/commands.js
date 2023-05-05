894fbe414b5bf671e115b9f9fc35cf4747afbf3d
GreDDySS <misha.sudos@yandex.ru>
2023-05-06 01:05:13 +0800

    get commands

--- utils/commands.js
+++ utils/commands.js
@@ -5,9 +5,9 @@ const FILES = fs.readdirSync(CMD_DIR)
 const commandsData = []
 
 for (const file of FILES) {
-    const cmd = require(CMD_DIR + "/" + file)
+    const cmd = require(`../commands/${file}`)
     const cfg = cmd.config
-    for (var i = 0; i < FILES.length; i++) {
+    
         commandsData.push({
             name: cfg.name,
             description: cfg.description,
@@ -15,7 +15,5 @@ for (const file of FILES) {
             active: cfg.active,
             admin: cfg.adminOnly
         })
-    }
 }
-
 module.exports = {commandsData}
