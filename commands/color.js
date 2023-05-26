6995dd0b9ca7c3edce36bbdda3c4667fb28dd0db
GreDDySS <misha.sudos@yandex.ru>
2023-05-26 20:57:01 +0800

    fix

--- commands/color.js
+++ commands/color.js
@@ -16,26 +16,24 @@ exports.run = async (client, chat, channel, commandData) => {
         }
     }
 
+
     let color = ""
     if (isColor === false) {
         let userColor = await greddBot.Utils.APITwitch.getLoginInfo(username)
         color = userColor.map((item) => {return item.chatColor.replace("#", "")})
     } else {
-        color = args[0]
+        color = args[0].substring(1)
     }
-
-    if (username === commandData.user.login) {
-        color = commandData.user.color
-    }
-
     const colorName = await greddBot.Utils.ApiClient.request(`www.thecolorapi.com/id?hex=${color}`)
 
     if (isColor === true) {
         client.say(channel, `@${commandData.user.name}, HEX цвет: ${colorName.name.value} ${color}`)
+    } else {
+        client.say(channel, `@${commandData.user.name}, Этот пользователь использует: ${colorName.name.value} ${color} `)
     }
 
-    client.say(channel, `@${commandData.user.name}, Этот пользователь использует: ${colorName.name.value} ${color} `)
 }
+
 module.exports.config = {
     name: "color",
     description: "Says the name of the user's color",
