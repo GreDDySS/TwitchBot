37d63484261d033c19a199d43147e5417bbe0bd1
GreDDySS <misha.sudos@yandex.ru>
2024-07-11 02:45:51 +0800

    Update list celebration

---
+++ commands/update.js
@@ -0,0 +1,15 @@
+const fs = require("fs")
+exports.run = async (client, chat, channel, commandData) => {
+    bot.Utils.celebration.getListCelebration().then(async () => {
+        const data = JSON.parse(fs.readFileSync("other/celebration.json", {encoding: "utf-8"}))
+        await chat.send(channel, `Обновление праздников завершено!`)   
+        chat.send(channel, `Загрузил ${data.length} праздник!`)})    
+}
+module.exports.config = {
+    name: "update",
+    description: "Update celebration",
+    cooldown: 5000,
+    aliases: [],
+    active: true,
+    adminOnly: true
+}
