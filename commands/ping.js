1f5e8cc02092845f70bb6952ee5bcdd8e4b31c5b
GreDDySS <misha.sudos@yandex.ru>
2024-07-11 02:43:27 +0800

    Check status bot

---
+++ commands/ping.js
@@ -0,0 +1,18 @@
+exports.run = async (client, chat, channel, commandData) => {
+    let dateNow = Date.now()
+    await client.ping();
+    let dateAfterPing = Date.now()
+    const used = process.memoryUsage().heapUsed / 1024 / 1024
+    const memory = Math.round(used * 100) / 100
+    const channelCount = await bot.Channel.getJoinable()
+    
+    chat.send(channel, `@${commandData.user.name} PONG! 🟢 Ping: ${dateAfterPing - dateNow}ms | Channels: ${channelCount.length} | Uptime: ${bot.Utils.misc.uptime()} | Memory: ${memory}MB/512MBs | Cmd executed: ${bot.Utils.temp.cmdCount}`)
+}
+module.exports.config = {
+    name: "ping",
+    description: "Check status bot",
+    cooldown: 5000,
+    aliases: ["ping"],
+    active: true,
+    adminOnly: false
+}
