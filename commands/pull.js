76afb12fb1bc0898a50b69ebc590626b4d4461f1
GreDDySS <misha.sudos@yandex.ru>
2024-07-11 02:44:16 +0800

    Pull the files from GitHub

---
+++ commands/pull.js
@@ -0,0 +1,14 @@
+const shell = require("child_process")
+exports.run = async (client, chat, channel, commandData) => {
+    await chat.send(channel, `🔄️ Pull files...`)
+    shell.exec("git pull")
+    shell.exec("npm i")
+}
+module.exports.config = {
+    name: "pull",
+    description: "Pull the files from github",
+    cooldown: 5000,
+    aliases: [],
+    active: true,
+    adminOnly: true
+}
