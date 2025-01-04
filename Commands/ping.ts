44aec41150b640129ac80f0f8520c86772a0fe6b
GreDDySS <misha.sudos@yandex.ru>
2025-01-04 14:00:37 +0800

    update command, add uptime bot

--- Commands/ping.ts
+++ Commands/ping.ts
@@ -17,7 +17,7 @@ const ping = {
     const memory = Math.round(used * 100) / 100
     const channelCount = await Channel.getJoinable()
     try {
-      await client.CommandUtils.send(commandData.channel, `@${commandData.user.name} PONG! 🟢 Ping: ${dateAfterPing - dateNow}ms • Channels: ${channelCount.length} · Uptime: soon... • Memory: ${memory}MB/512MBs • cmdUsed: ${client.Temp.cmdCount}`);
+      await client.CommandUtils.send(commandData.channel, `@${commandData.user.name} PONG! 🟢 Ping: ${dateAfterPing - dateNow}ms • Channels: ${channelCount.length} · Uptime: ${client.Utils.uptime()} • Memory: ${memory}MB/512MBs • cmdUsed: ${client.Temp.cmdCount}`);
     } catch (error) {
       Logger.error(`Error executing command ${ping.name}: ${error}`);
     }
