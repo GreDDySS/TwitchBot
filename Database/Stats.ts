c724a81ee4cb4a027069c1b786f44de4f32f75b3
GreDDySS <misha.sudos@yandex.ru>
2025-01-04 21:26:08 +0800

    new function getStatistics

--- Database/Stats.ts
+++ Database/Stats.ts
@@ -3,6 +3,7 @@ import { query } from "../Modules/Database";
 import { Logger } from "../Modules/Logger";
 import type { channelStats } from "../types";
 import pc from "picocolors"
+import { uptime } from "../utils/Utils"
 
 const buffer: Record<string, channelStats> = {};
 const flushInterval = 5000 // 5 seconds
@@ -42,5 +43,35 @@ export class Stats {
   static async incrementCommand(channelID: string): Promise<void> {
     if (!buffer[channelID]) buffer[channelID] = { messages: 0, commands: 0 };
     buffer[channelID].commands++;
+  }
+
+  static async getStatistics(): Promise<string> {
+    try {
+      // Пример запросов к базе данных
+      const totalChannels = await query(
+        `SELECT COUNT(*) FROM "Channels" WHERE "ignore" = false`
+      );
+      const totalMessages = await query(
+        `SELECT SUM(messages) AS Total FROM "Stats"`
+      );
+      const memoryUsage = process.memoryUsage();
+
+      // Форматируем статистику для вывода
+      const formattedStatistics = `
+          📊 **Статистика Бота**:
+          - 🟢 Активных каналов: ${totalChannels.rows[0].count}
+          - ✉️ Сообщений обработано: ${totalMessages.rows[0].total || 0}
+          - ⏳ Аптайм: ${uptime()} 
+          - 💾 Использование памяти:
+            - RSS: ${(memoryUsage.rss / 1024 / 1024).toFixed(2)} MB
+            - Heap Total: ${(memoryUsage.heapTotal / 1024 / 1024).toFixed(2)} MB
+            - Heap Used: ${(memoryUsage.heapUsed / 1024 / 1024).toFixed(2)} MB
+          `;
+
+      return formattedStatistics;
+    } catch (error) {
+      Logger.error(`Ошибка получения статистики: ${(error as Error).message}`);
+      throw new Error("Ошибка получения статистики");
+    }
   }
 }
