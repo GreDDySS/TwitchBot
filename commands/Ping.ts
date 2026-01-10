276351e56263ed9baf4894d6aac49699379e75c2
GreDDySS <misha.sudos@yandex.ru>
2026-01-11 03:45:40 +0800

    add more information

--- commands/Ping.ts
+++ commands/Ping.ts
@@ -1,3 +1,4 @@
+import { statsStore } from "../Utils/StatsStore";
 import type { ICommand } from "../Utils/types";
 
 export default {
@@ -7,6 +8,8 @@ export default {
     cooldown: 5,
     
     async execute({send, reply, user, text, args, msg}) {
-        await send(`@${user}, PONG!`)
+        const stats = await statsStore.getStats()
+        
+        await send(`@${user}, PONG! · Uptime: ${stats["uptime"]} · Messages: ${stats["messages"]} · Cmd: ${stats["commands"]} · Channels: ${stats["channels"]}`)
     }
 } as ICommand
