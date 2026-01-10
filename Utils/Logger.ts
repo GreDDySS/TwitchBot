5d5695494f8a00c27ef0623839d279da50af536d
GreDDySS <misha.sudos@yandex.ru>
2026-01-11 03:44:26 +0800

    feat(logger): add database error transport with automatic module tag parsing

--- Utils/Logger.ts
+++ Utils/Logger.ts
@@ -2,30 +2,50 @@ import winston from "winston";
 import Transport from "winston-transport"
 import { logStore } from "./LogStore";
 import { statsStore } from "./StatsStore";
+import { createErrorLog } from "../Database/ErrorLogs";
+import { RuntimeConfig } from "../Config/RuntimeConfig";
 
 
 class InkTransport extends Transport {
-   override log(info: any, callback: () => void) {
-    statsStore.incrementError();
-    logStore.addLog(info);
-    callback();
-  }
+    override log(info: any, callback: () => void) {
+        statsStore.incrementError();
+        logStore.addLog(info);
+        callback();
+    }
 }
 
-const logFormat = winston.format.combine(
-    winston.format.timestamp({ format: "HH:mm:ss" }),
-    winston.format.printf(({ timestamp, level, message}) => {
-        return `${timestamp} - [${level.toUpperCase()}] || ${message}`
-    })
-);
+class DatabaseErrorTransport extends Transport {
+    override log(info: any, callback: () => void) {
+        if (info.level === 'error') {
+            const stack = info.stack || (info.metadata && info.metadata.stack);
+            
+            const moduleMatch = info.message.match(/^\[([^\]]+)\]\s*(.*)/);
+
+            const name = moduleMatch ? moduleMatch[1] : "GreDDBot";
+            const cleanMessage = moduleMatch ? moduleMatch[2] : info.message;
+            
+            if (!RuntimeConfig.disableDbWrites) {
+                createErrorLog(
+                    name,
+                    cleanMessage,
+                    stack
+                );
+            }
+        }
+        callback();
+    }
+}
 
 export const Logger = winston.createLogger({
-    level: "info",
-    format: logFormat,
+    level: "debug",
+    format: winston.format.combine(
+        winston.format.timestamp({ format: "HH:mm:ss" }),
+        winston.format.printf(({ timestamp, level, message }) => {
+            return `${timestamp} - [${level.toUpperCase()}] || ${message}`
+        })
+    ),
     transports: [
-
-        //TODO: Maybe log in files?
-        
         new InkTransport(),
+        new DatabaseErrorTransport()
     ]
 })
