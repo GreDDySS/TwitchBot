6c0ea80f771df505cbae14b5ca3c3c9fe8d02ec6
GreDDySS <misha.sudos@yandex.ru>
2026-01-11 03:45:15 +0800

    feat(cli): integrate meow for CLI flags and add headless mode support

--- index.tsx
+++ index.tsx
@@ -2,37 +2,50 @@ import React from 'react';
 import { render } from 'ink';
 import App from './ui/App';
 import config from './Config/config'
+import { RuntimeConfig } from './Config/RuntimeConfig';
 import { startTwitch } from './Clients/Twitch';
 import meow from "meow"
+import { sleep } from 'bun';
 
 const cli = meow(`
     Usage
       $ bun run index.tsx
 
     Options
-      --no-ui   Disable TUI (useful for Docker Logs)
-      --help    Show this help
+      --no-ui           Disable TUI (useful for Docker Logs)
+      --help            Show this help
+      --no-db-writes    Disable all Database writes (Logs, Users, Error)
     
     Examples
       $ bun run index.tsx --no-ui
     `, {
-        importMeta: import.meta,
-        flags: {
-            ui: {
-                type: 'boolean',
-                default: true
-            },
+    importMeta: import.meta,
+    flags: {
+        ui: {
+            type: 'boolean',
+            default: true
+        },
+        disableDbWrites: {
+            type: 'boolean',
+            default: false
         }
     }
+}
 );
 
 async function main() {
-    console.clear()
+    RuntimeConfig.disableDbWrites = cli.flags.disableDbWrites;
+    if (RuntimeConfig.disableDbWrites) {
+        console.warn("\n[WARNING] 🛑 DATABASE WRITES ARE DISABLED! \n");
+        sleep(3000);
+    }
     
-    if (cli.flags.ui){
+    console.clear()
+
+    if (cli.flags.ui) {
         const { unmount } = render(<App config={config} />);
-        
-        
+
+
         process.on("SIGINT", () => {
             unmount();
             process.exit(0);
@@ -40,7 +53,7 @@ async function main() {
     } else {
         console.log("Starting in Headles Mode (No UI)...");
     }
-    
+
     startTwitch().catch(err => {
         console.error("Fatal Error: ", err)
         process.exit(1);
